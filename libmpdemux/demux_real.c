soulic/mplayer

commit e8a29d19cbf23087b7586e9f0047e389789e4371

Convert all if(verbose>X) to mp_msg_test calls.
git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@17933 b3059339-0415-0410-9bf9-f77b7e298cf2

libmpdemux/demux_real.c
@@ -159,7 +159,7 @@ static void dump_index(demuxer_t *demuxer, int stream_id)
     real_index_table_t *index;
     int i, entries;
 
-    if (verbose<=1)
+    if ( mp_msg_test(MSGT_DEMUX,MSGL_V) )
 	return;
     
     if (stream_id >= MAX_STREAMS)
@@ -1448,7 +1448,7 @@ static demuxer_t* demux_open_real(demuxer_t* demuxer)
 		    sh->wf->wFormatTag = sh->format;
 		    
 		    mp_msg(MSGT_DEMUX,MSGL_V,"audio fourcc: %.4s (%x)\n", (char *)&sh->format, sh->format);
-		    if (verbose > 0)
+		    if ( mp_msg_test(MSGT_DEMUX,MSGL_V) )
 		    print_wave_header(sh->wf);
 
 		    /* Select audio stream with highest bitrate if multirate file*/

libao2/ao_sun.c
@@ -64,7 +64,6 @@ static enum {
     RTSC_DISABLED
 } enable_sample_timing;
 
-extern int verbose;
 
 
 // convert an OSS audio format specification into a sun audio encoding
@@ -127,19 +126,19 @@ static int realtime_samplecounter_available(char *dev)
     info.play.encoding = AUDIO_ENCODING_LINEAR;
     info.play.samples = 0;
     if (ioctl(fd, AUDIO_SETINFO, &info)) {
-	if (verbose>0)
+	if ( mp_msg_test(MSGT_AO,MSGL_V) )
 	    mp_msg(MSGT_AO, MSGL_ERR, MSGTR_AO_SUN_RtscSetinfoFailed);
 	goto error;
     }
     
     if (write(fd, silence, len) != len) {
-	if (verbose>0)
+	if ( mp_msg_test(MSGT_AO,MSGL_V) )
 	    mp_msg(MSGT_AO, MSGL_ERR, MSGTR_AO_SUN_RtscWriteFailed);
 	goto error;
     }
 
     if (ioctl(fd, AUDIO_GETINFO, &info)) {
-	if (verbose>0)
+	if ( mp_msg_test(MSGT_AO,MSGL_V) )
 	    perror("rtsc: GETINFO1");
 	goto error;
     }
@@ -161,18 +160,18 @@ static int realtime_samplecounter_available(char *dev)
 	    break;
 
 	if (ioctl(fd, AUDIO_GETINFO, &info)) {
-	    if (verbose>0)
+	    if ( mp_msg_test(MSGT_AO,MSGL_V) )
 		perror("rtsc: GETINFO2 failed");
 	    goto error;
 	}
 	if (info.play.samples < last_samplecnt) {
-	    if (verbose>0)
+	    if ( mp_msg_test(MSGT_AO,MSGL_V) )
 		printf("rtsc: %d > %d?\n", last_samplecnt, info.play.samples);
 	    goto error;
 	}
 
 	if ((increment = info.play.samples - last_samplecnt) > 0) {
-	    if (verbose>0)
+	    if ( mp_msg_test(MSGT_AO,MSGL_V) )
 		printf("ao_sun: sample counter increment: %d\n", increment);
 	    if (increment < min_increment) {
 		min_increment = increment;
@@ -195,7 +194,7 @@ static int realtime_samplecounter_available(char *dev)
     if (min_increment < 2000)
 	rtsc_ok = RTSC_ENABLED;
 
-    if (verbose>0)
+    if ( mp_msg_test(MSGT_AO,MSGL_V) )
 	printf("ao_sun: minimum sample counter increment per 10msec interval: %d\n"
 	       "\t%susing sample counter based timing code\n",
 	       min_increment, rtsc_ok == RTSC_ENABLED ? "" : "not ");

libmpcodecs/ad_acm.c
@@ -74,7 +74,7 @@ static int preinit(sh_audio_t *sh_audio)
 //    priv->o_wf->wBitsPerSample = inf_fmt->wBitsPerSample;
     priv->o_wf->cbSize = 0;
     
-    if (verbose>0)
+    if ( mp_msg_test(MSGT_DECAUDIO,MSGL_V) )
     {
 	mp_msg(MSGT_DECAUDIO, MSGL_V, "Input format:\n");
 	print_wave_header(in_fmt);
@@ -98,7 +98,7 @@ static int preinit(sh_audio_t *sh_audio)
     mp_msg(MSGT_WIN32, MSGL_V, "Audio codec opened OK! ;-)\n");
     
     acmStreamSize(priv->handle, in_fmt->nBlockAlign, &srcsize, ACM_STREAMSIZEF_SOURCE);
-    //if (verbose) printf("Audio ACM output buffer min. size: %ld (reported by codec)\n", srcsize);
+    //if ( mp_msg_test(MSGT_DECAUDIO,MSGL_V) ) printf("Audio ACM output buffer min. size: %ld (reported by codec)\n", srcsize);
     srcsize *= 2;
     //if (srcsize < MAX_OUTBURST) srcsize = MAX_OUTBURST;
     if (!srcsize)

libmpcodecs/ad_twin.c
@@ -110,11 +110,11 @@ static int init_vqf_audio_codec(sh_audio_t *sh_audio){
     priv->o_wf.wBitsPerSample=in_fmt->wBitsPerSample;
     priv->o_wf.cbSize=0;
 
-    if(verbose)
+    if( mp_msg_test(MSGT_DECAUDIO,MSGL_V) )
     {
-    mp_msg(MSGT_DECAUDIO, MSGL_INFO, "Input format:\n");
+    mp_msg(MSGT_DECAUDIO, MSGL_V, "Input format:\n");
     print_wave_header(in_fmt);
-    mp_msg(MSGT_DECAUDIO, MSGL_INFO, "Output fmt:\n");
+    mp_msg(MSGT_DECAUDIO, MSGL_V, "Output fmt:\n");
     print_wave_header(&priv->o_wf);
     }
     memcpy(&priv->hi,&in_fmt[1],sizeof(headerInfo));

libmpcodecs/ae_lame.c
@@ -70,7 +70,6 @@ m_option_t lameopts_conf[]={
 
 
 static int pass;
-extern int verbose;
 
 static int bind_lame(audio_encoder_t *encoder, muxer_stream_t *mux_a)
 {
@@ -192,7 +191,7 @@ int mpae_init_lame(audio_encoder_t *encoder)
         mp_msg(MSGT_MENCODER, MSGL_FATAL, MSGTR_LameCantInit); 
         return 0;
     }
-    if(verbose>0) {
+    if( mp_msg_test(MSGT_MENCODER,MSGL_V) ) {
         lame_print_config(lame);
         lame_print_internals(lame);
     }

libmpcodecs/vd.c
@@ -174,7 +174,7 @@ int mpcodecs_config_vo(sh_video_t *sh, int w, int h, unsigned int preferred_outf
     // check if libvo and codec has common outfmt (no conversion):
 csp_again:
 
-    if(verbose>0){
+    if( mp_msg_test(MSGT_DECVIDEO,MSGL_V) ){
 	vf_instance_t* f=vf;
 	mp_msg(MSGT_DECVIDEO,MSGL_V,"Trying filter chain:");
 	for(f = vf ; f ; f = f->next)

libmpcodecs/vd_ffmpeg.c
@@ -1001,7 +1001,7 @@ static int mc_get_buffer(AVCodecContext *avctx, AVFrame *pic){
     }
     assert(avctx->draw_horiz_band == mc_render_slice);
     assert(avctx->release_buffer == mc_release_buffer);
-    if(verbose > 4)
+    if( mp_msg_test(MSGT_DECVIDEO,MSGL_DBG5) )
         printf("vd_ffmpeg::mc_get_buffer\n");
 
     if(init_vo(sh,avctx->pix_fmt) < 0){
@@ -1068,7 +1068,7 @@ static int mc_get_buffer(AVCodecContext *avctx, AVFrame *pic){
     pic->type= FF_BUFFER_TYPE_USER;
 
     render=(xvmc_render_state_t*)mpi->priv;//same as data[2]
-    if(verbose > 4)
+    if( mp_msg_test(MSGT_DECVIDEO,MSGL_DBG5) )
         printf("vd_ffmpeg::mc_get_buffer (render=%p)\n",render);
     assert(render != 0);
     assert(render->magic == MP_XVMC_RENDER_MAGIC);
@@ -1095,7 +1095,7 @@ static void mc_release_buffer(AVCodecContext *avctx, AVFrame *pic){
 //printf("R%X %X\n", pic->linesize[0], pic->data[0]);
 //mark the surface as not requared for prediction
     render=(xvmc_render_state_t*)pic->data[2];//same as mpi->priv
-    if(verbose > 4)
+    if( mp_msg_test(MSGT_DECVIDEO,MSGL_DBG5) )
         printf("vd_ffmpeg::mc_release_buffer (render=%p)\n",render);
     assert(render!=NULL);
     assert(render->magic==MP_XVMC_RENDER_MAGIC);

libmpcodecs/vd_vfw.c
@@ -241,9 +241,9 @@ static int init(sh_video_t *sh){
 	set_csp(priv->o_bih,sh->codec->outfmt[sh->outfmtidx]);
 
     mp_msg(MSGT_WIN32, MSGL_V, "Input format:\n");
-    if(verbose>0) print_video_header(sh->bih);
+    if( mp_msg_test(MSGT_HEADER,MSGL_V) ) print_video_header(sh->bih);
     mp_msg(MSGT_WIN32, MSGL_V, "Output format:\n");
-    if(verbose>0) print_video_header(priv->o_bih);
+    if( mp_msg_test(MSGT_HEADER,MSGL_V) ) print_video_header(priv->o_bih);
 
     // set postprocessing level in xvid/divx4 .dll
     ICSendMessage(priv->handle, ICM_USER+80, (long)(&divx_quality), 0);

commit 2d18a153016a5e99d04839785ae68cbf8bcd51d6

Fix a regression caused by r17933; RealMedia index tables could never be printed.

libmpdemux/demux_real.c
@@ -140,7 +140,7 @@ static void dump_index(demuxer_t *demuxer, int stream_id)
     real_index_table_t *index;
     int i, entries;
 
-    if ( mp_msg_test(MSGT_DEMUX,MSGL_V) )
+    if (!mp_msg_test(MSGT_DEMUX,MSGL_V))
 	return;
     
     if ((unsigned)stream_id >= MAX_STREAMS)