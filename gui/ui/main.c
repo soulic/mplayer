soulic/mplayer

commit 2d7c9b336d547274eaac01f860a9edd02ef47839

Don't unnecessarily and annoyingly move the video window.
So far, this has been done at the start and end of a video (in
non-fullscreen mode) as well as when selecting half, normal or
double size of the video.

Don't position the video window to the values given in the skin file,
because the user might have moved it in the meantime, so just let it
where it currently is.

This closes Bugzilla #1355.


git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@35757 b3059339-0415-0410-9bf9-f77b7e298cf2

gui/ui/main.c
@@ -342,7 +342,6 @@ NoPause:
             uiFullScreen();
            }
           wsResizeWindow( &guiApp.videoWindow, guiInfo.VideoWidth / 2, guiInfo.VideoHeight / 2 );
-          wsMoveWindow( &guiApp.videoWindow, False, guiApp.video.x, guiApp.video.y );
           btnSet( evFullScreen,btnReleased );
          }
         break;
@@ -354,7 +353,6 @@ NoPause:
             uiFullScreen();
            }
           wsResizeWindow( &guiApp.videoWindow, guiInfo.VideoWidth * 2, guiInfo.VideoHeight * 2 );
-          wsMoveWindowWithin( &guiApp.videoWindow, False, guiApp.video.x, guiApp.video.y );
           btnSet( evFullScreen,btnReleased );
          }
         break;
@@ -366,7 +364,6 @@ NoPause:
             uiFullScreen();
            }
           wsResizeWindow( &guiApp.videoWindow, guiInfo.VideoWidth, guiInfo.VideoHeight );
-          wsMoveWindow( &guiApp.videoWindow, False, guiApp.video.x, guiApp.video.y );
           btnSet( evFullScreen,btnReleased );
 	  break;
          } else if ( !guiApp.videoWindow.isFullScreen ) break;
@@ -375,10 +372,7 @@ NoPause:
          {
           uiFullScreen();
           if ( !guiApp.videoWindow.isFullScreen )
-           {
             wsResizeWindow( &guiApp.videoWindow, iparam ? guiInfo.VideoWidth : guiApp.video.width, iparam ? guiInfo.VideoHeight : guiApp.video.height );
-            wsMoveWindow( &guiApp.videoWindow, False, guiApp.video.x, guiApp.video.y );
-           }
          }
 	if ( guiApp.videoWindow.isFullScreen ) btnSet( evFullScreen,btnPressed );
 	 else btnSet( evFullScreen,btnReleased );

gui/interface.c
@@ -780,9 +780,6 @@ int gui(int what, void *data)
         if (!guiApp.videoWindow.isFullScreen || !guiApp.videoWindow.Mapped) {
             if (!guiApp.videoWindow.isFullScreen)
                 wsResizeWindow(&guiApp.videoWindow, guiInfo.VideoWidth, guiInfo.VideoHeight);
-
-            wsMoveWindow(&guiApp.videoWindow, False, guiApp.video.x, guiApp.video.y);
-
             if (!guiApp.videoWindow.Mapped)
                 wsVisibleWindow(&guiApp.videoWindow, wsShowWindow);
         }
@@ -861,10 +858,8 @@ int gui(int what, void *data)
             if (gtkShowVideoWindow) {
                 guiInfo.VideoWindow = True;
 
-                if (!guiApp.videoWindow.isFullScreen) {
+                if (!guiApp.videoWindow.isFullScreen)
                     wsResizeWindow(&guiApp.videoWindow, guiApp.video.width, guiApp.video.height);
-                    wsMoveWindow(&guiApp.videoWindow, False, guiApp.video.x, guiApp.video.y);
-                }
 
                 if (!guiApp.videoWindow.Mapped)
                     wsVisibleWindow(&guiApp.videoWindow, wsShowWindow);

commit 135737fb4cb7ced94cea9773a73efd2094579f66

Fix bug introduced in r35757.
(A double size video is supposed to change its position
in order to fit into the screen, see r34086.)

gui/ui/main.c
@@ -353,6 +353,7 @@ NoPause:
             uiFullScreen();
            }
           wsResizeWindow( &guiApp.videoWindow, guiInfo.VideoWidth * 2, guiInfo.VideoHeight * 2 );
+          wsMoveWindowWithin( &guiApp.videoWindow, False, guiApp.video.x, guiApp.video.y );
           btnSet( evFullScreen,btnReleased );
          }
         break;